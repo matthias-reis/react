ac44ddf3f0af7b896c3f796e6f479b3d57465465
Matthias Reis <matthias.reis@xing.com>
2015-11-23 18:55:38 +0100

    chapter 3

--- build/03/App.jsx
+++ build/03/App.jsx
@@ -1,9 +1,24 @@
-const fn = () => console.log('log');
+'use strict';
 
-fn();
-
-class App {
+class App extends React.Component {
   constructor() {
-    console.log('log2');
+    super();
+    this.state = {
+      counter: 0
+    };
+    window.setInterval(function() {
+      this.setState({counter: this.state.counter + 1});
+    }.bind(this), 1000);
+  }
+
+  render() {
+    return (
+      <p>Time spent on this page: <strong>{this.state.counter} s</strong></p>
+    )
   }
 }
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('xi-react')
+);
