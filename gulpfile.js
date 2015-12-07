44b5cad547bc19fcbd2c48b09356de34175b01ca
Stefan Imhoff <stefan.imhoff@xing.com>
2015-12-07 14:19:52 +0100

    Improve gulp task, add missing modules

--- gulpfile.js
+++ gulpfile.js
@@ -5,45 +5,46 @@ var sass = require("gulp-sass");
 var rename = require("gulp-rename");
 
 gulp.task("js", function() {
-	return gulp.src("build/**/*.jsx")
-	.pipe(babel({presets: ['es2015', 'react']}).on('error', function (error) {
-		console.log(error.message);
+  return gulp.src("build/**/*.jsx")
+  .pipe(babel({presets: ['es2015', 'react']}).on('error', function (error) {
+    console.log(error.message);
     this.emit('end');
-	})).pipe(gulp.dest("build"));
+  })).pipe(gulp.dest("build"));
 });
 
 gulp.task("css", function() {
-	return gulp.src("scss/*.scss")
-	.pipe(sass({
-		// outputStyle: 'expanded',
-		outputStyle: 'compressed',
-		includePaths: ['./node_modules/compass-mixins/lib']
-	}).on('error', sass.logError))
-	.pipe(gulp.dest('./build/css'));
+  return gulp.src("scss/*.scss")
+  .pipe(sass({
+    // outputStyle: 'expanded',
+    outputStyle: 'compressed',
+    includePaths: ['./node_modules/compass-mixins/lib']
+  }).on('error', sass.logError))
+  .pipe(gulp.dest('./build/css'));
 });
 
 
 gulp.task('run', function() {
-	gulp.src('')
-	.pipe(webserver({
-		livereload: {
-			enabled: true
-		},
-		fallback: 'index.html',
-		directoryListing: {
-			enable: true,
-			path: ''
-		},
-		port: 4200,
-		host: '0.0.0.0',
-		open: false
-	}));
+  gulp.src('')
+  .pipe(webserver({
+    livereload: {
+      enabled: true
+    },
+    fallback: 'index.html',
+    directoryListing: {
+      enable: true,
+      path: ''
+    },
+    port: 4200,
+    host: '0.0.0.0',
+    open: false
+  }));
 });
-
 
 gulp.task('compile', ['js', 'css']);
 
 gulp.task('watch', function() {
-	gulp.watch('**/*.scss', ['css']);
-	gulp.watch('build/**/*.jsx', ['js']);
+  gulp.watch('**/*.scss', ['css']);
+  gulp.watch('build/**/*.jsx', ['js']);
 });
+
+gulp.task('default', ['run', 'watch']);
